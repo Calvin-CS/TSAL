6ba5b412a037ffb82eaa7ebc618e922531d66590
Mark Wissink <markcwissink@gmail.com>
2019-06-07 14:59:19 -0400

    Chaning file structure

--- src/main.cpp
+++ src/main.cpp
@@ -17,15 +17,14 @@ void thread_sleep(unsigned milliseconds) {
   std::this_thread::sleep_for(std::chrono::milliseconds(milliseconds));
 }
 
-const unsigned problemSize = 300;
-const unsigned numThreads = 4;
+unsigned problemSize = 500;
+unsigned numThreads = 1;
 
 int main() {  
   
-  omp_set_num_threads(numThreads);
   
   Mixer mixer;
-
+  /*
  // mixer.add(&chord);
   Channel chan;
   
@@ -37,14 +36,14 @@ int main() {
   osc.setNote(C4);
   
   mixer.add(&chan);
-  
+ 
   chan.add(&osc);
 
   thread_sleep(1000);
   osc.setGain(0.2);
   thread_sleep(1000);
   
-  chan.remove(&osc);
+  //chan.remove(&osc);
   
   thread_sleep(1000);
   
@@ -55,6 +54,7 @@ int main() {
   Chord chord(numThreads, problemSize);
   chan.add(&chord);
   
+  omp_set_num_threads(numThreads);
   #pragma omp parallel
   {
     unsigned id = omp_get_thread_num();
@@ -70,6 +70,45 @@ int main() {
     thread_sleep(1000);
   }
   chord.stop();
+
+  numThreads = 4;
+   Chord chord1(numThreads, problemSize);
+  chan.add(&chord1);
   
+  omp_set_num_threads(numThreads);
+  #pragma omp parallel
+  {
+    unsigned id = omp_get_thread_num();
+
+    chord1.start(id);
+    thread_sleep(1000);
+    
+    #pragma omp for
+    for(unsigned i = 1; i <= problemSize; i++) {
+      thread_sleep(10); 
+      chord1.step(id);
+    }
+    thread_sleep(1000);
+  }
+  chord1.stop();
+  */
+  printf("\nBeginning\n");
+
+  #pragma omp parallel 
+  printf("\nPart I");
+
+  printf("\n\nBetween I and II...\n");
+
+  omp_set_num_threads(3);
+
+  #pragma omp parallel 
+  printf("\nPart II...");
+
+  printf("\n\nBetween II and III...\n");
+
+  #pragma omp parallel num_threads(5)
+  printf("\nPart III...");
+  
+  printf("\n");
   return 0;
 }
