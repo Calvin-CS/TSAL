12ec8eae8937746e76f03a6c74c89fd5db2115ce
mcwissink <markcwissink@gmail.com>
2020-02-24 14:59:19 -0500

    removed soundfont and midifile from dependencies

--- include/tsal.hpp
+++ include/tsal.hpp
@@ -4,13 +4,10 @@
 #include "Channel.hpp"
 #include "Compressor.hpp"
 #include "Delay.hpp"
-/* #include "MidiNotes.hpp" */
 #include "Mixer.hpp"
-/* #include "MidiParser.hpp" */
 #include "Oscillator.hpp"
 #include "PolySynth.hpp"
 #include "ProgressOctave.hpp"
-#include "SoundFont.hpp"
 #include "Synth.hpp"
 #include "ThreadSynth.hpp"
 #include "Timing.hpp"
