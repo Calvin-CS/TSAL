77ee166f21521c63ecd6d7b3d75f73295322fe91
RyanVreeke <rvreeke0229@gmail.com>
2021-07-22 18:56:44 +0100

    Edited install scripts and the tsal.hpp file.

--- include/tsal.hpp
+++ include/tsal.hpp
@@ -1,18 +1,18 @@
 #ifndef TSAL_H
 #define TSAL_H
 
-#include "Channel.hpp"
-#include "Compressor.hpp"
-#include "Delay.hpp"
-#include "MidiNotes.hpp"
-#include "Mixer.hpp"
-#include "MidiParser.hpp"
-#include "Oscillator.hpp"
-#include "PolySynth.hpp"
-#include "ProgressOctave.hpp"
-#include "SoundFont.hpp"
-#include "Synth.hpp"
-#include "ThreadSynth.hpp"
-#include "Timing.hpp"
+#include "TSAL/Channel.hpp"
+#include "TSAL/Compressor.hpp"
+#include "TSAL/Delay.hpp"
+#include "TSAL/MidiNotes.hpp"
+#include "TSAL/Mixer.hpp"
+#include "TSAL/MidiParser.hpp"
+#include "TSAL/Oscillator.hpp"
+#include "TSAL/PolySynth.hpp"
+#include "TSAL/ProgressOctave.hpp"
+#include "TSAL/SoundFont.hpp"
+#include "TSAL/Synth.hpp"
+#include "TSAL/ThreadSynth.hpp"
+#include "TSAL/Timing.hpp"
 
 #endif
